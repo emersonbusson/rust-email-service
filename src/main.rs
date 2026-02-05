1a316cc443ec47d7c0e4cd764e6dfa4a9747e284
emersonbusson <emersonbusson@gmail.com>
2026-02-05 10:49:13 -0300

    feat(server): init actix server with health_check integration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use rust_email_service::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = "127.0.0.1:8000";
+
+    let listener = TcpListener::bind(address)?;
+
+    run(listener)?.await
 }
